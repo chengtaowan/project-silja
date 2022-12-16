73185eb5e4ffe07122e48294246e33e50b3ab771
3llena <3llena@protonmail.com>
2022-12-16 07:48:15 +0100

    added .silja sym walking will add uti functions for loading syms next

--- sdk/include/type/sym.hxx
+++ sdk/include/type/sym.hxx
@@ -9,22 +9,21 @@ namespace sdk {
    struct sym_header_t {
       std::int16_t m_magic;
       std::int16_t m_next;
-      std::int64_t m_path;
+      char m_path[0x1];
 
       [[ nodiscard ]]
-      constexpr bool is_valid() {
+      constexpr bool is_valid( ) {
          return m_magic == sym_magic_t::header;
       }
    };
 
    struct sym_record_t {
       std::int16_t m_magic;
-      std::int64_t m_name;
-      std::int64_t m_parent;
       std::int32_t m_rva;
+      std::int64_t m_next;
 
       [[ nodiscard ]]
-      constexpr bool is_valid() {
+      constexpr bool is_valid( ) {
          return m_magic == sym_magic_t::record;
       }
    };
